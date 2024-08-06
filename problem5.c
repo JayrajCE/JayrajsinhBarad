07609c4f5467b0d696c8774e54d49bf455a813bf
JayrajCE <jayrajsinh.r.barad@gmail.com>
2024-08-06 23:56:55 +0530

    new

---
+++ problem5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    float c , fh ;
+    printf("Enter th temprature in degree celsius :");
+    scanf("%f" , &c);
+    fh = (float)9/5*c + 32;
+    printf("The temprature in fahreneit is %f" , fh);
+}
