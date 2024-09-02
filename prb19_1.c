7fc47d10ecd940a2c244bf24702aa701695236d3
JayrajCE <jayrajsinh.r.barad@gmail.com>
2024-09-02 16:10:41 +0530

    Add files via upload

---
+++ prb19_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int   lenght=0 , i ;
+    char str[50];
+    printf("Enter your note\n");
+    scanf("%s" , str);
+    for(i=0 ; str[i] != '\0' ; i++)
+    {
+        lenght++;
+    }
+    printf("Lenght of string is %d"  , lenght);
+}
